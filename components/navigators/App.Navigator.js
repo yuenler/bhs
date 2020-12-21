f56c7be9ae77c56005d85bad488b8e317d32a91d
yuenler <lerchow@gmail.com>
2020-12-21 12:27:19 -0500

    Add sexy icons

--- components/navigators/App.Navigator.js
+++ components/navigators/App.Navigator.js
@@ -4,18 +4,48 @@ import ScheduleNavigator from './Schedule.Navigator';
 import MessagesNavigator from './Messages.Navigator';
 import EventsNavigator from './Events.Navigator';
 import FriendsNavigator from './Friends.Navigator';
+import { Ionicons } from '@expo/vector-icons';
+
 
 const Tabs = createBottomTabNavigator();
 
 export default class AppNavigator extends React.Component {
   render() {
     return (
-      <Tabs.Navigator>
+
+      <Tabs.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Schedule') {
+              iconName = 'ios-time';
+            } else if (route.name === 'Messages') {
+              iconName = 'ios-chatbubbles';
+            }
+            else if (route.name === 'Events') {
+              iconName = 'ios-happy';
+            }
+            else if (route.name === 'Friends') {
+              iconName = 'ios-people';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+
         <Tabs.Screen name="Schedule" component={ScheduleNavigator} />
         <Tabs.Screen name="Messages" component={MessagesNavigator} />
         <Tabs.Screen name="Events" component={EventsNavigator} />
         <Tabs.Screen name="Friends" component={FriendsNavigator} />
       </Tabs.Navigator>
+
     )
   }
 }
