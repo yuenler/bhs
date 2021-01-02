2120ff849f108ed50c7affd623258cdaf1156f5f
Allen316 <74190502+Allen316@users.noreply.github.com>
2021-01-02 17:15:56 -0500

    Update Schedule.Screen.js
    Allowed block name customization

--- components/screens/Schedule.Screen.js
+++ components/screens/Schedule.Screen.js
@@ -4,13 +4,270 @@ import { ScrollView } from 'react-native-gesture-handler';
 import Block from '../schedule/Block';
 
 export default class ScheduleScreen extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {isCustomized: false,
+			block: {
+			'A' : '' , 
 
-	state = {isCustomized: false}
+			'B' : '' , 
+
+			'C' : '' , 
+
+			'D' : '' , 
+
+			'E' : '' , 
+
+			'F' : '' , 
+
+			'G' : ''
+		  }}
+	}
+
+	  static getDerivedStateFromProps(props, state) {
+		  if (props.route.params && state.block[props.route.params.block] !== props.route.params.teacher){
+			let newBlock = {...state.block,
+				[props.route.params.block] : props.route.params.teacher
+			}
+			  return ({block:newBlock})
+		  }
+
+		return null;
+	  }
 
 	scrollRef = null;
 	block = 0;
 
 	render() {
+		const schedule = [
+			[
+				{
+					title: 'A  ' + this.state.block['A'],
+					starts: '8:20',
+					ends: '9:40 am',
+					color: 'A',
+					numbers: {
+						starts: 820,
+						ends: 940,
+						duration: 80
+					}
+				},
+				{
+					title: 'B  ' + this.state.block['B'],
+					starts: '9:55',
+					ends: '11:15 am',
+					color: 'B',
+					numbers: {
+						starts: 955,
+						ends: 1115,
+						duration: 80
+					}
+				},
+				{
+					title: 'C - 1/2  ' + this.state.block['C'],
+					starts: '11:30',
+					ends: '12:05 pm',
+					color: 'C',
+					numbers: {
+						starts: 1130,
+						ends: 1205,
+						duration: 35
+					}
+				},
+				{
+					title: 'Lunch',
+					starts: '12:10',
+					ends: '12:40 pm',
+					color: 'L',
+					numbers: {
+						starts: 1210,
+						ends: 1240,
+						duration: 30
+					}
+				},
+				{
+					title: 'C - 2/2  ' + this.state.block['C'],
+					starts: '12:45',
+					ends: '1:25 pm',
+					color: 'C',
+					numbers: {
+						starts: 1245,
+						ends: 1325,
+						duration: 40
+					}
+				},
+				{
+					title: 'D  ' + this.state.block['D'],
+					starts: '1:40',
+					ends: '3:00 pm',
+					color: 'D',
+					numbers: {
+						starts: 1340,
+						ends: 1500,
+						duration: 80
+					}
+				}
+			],
+			[
+				{
+					title: 'E  ' + this.state.block['E'],
+					starts: '8:20',
+					ends: '9:40 am',
+					color: 'E',
+					numbers: {
+						starts: 820,
+						ends: 940,
+						duration: 80
+					}
+				},
+				{
+					title: 'Advisory',
+					starts: '9:55',
+					ends: '10:30 am',
+					color: 'T',
+					numbers: {
+						starts: 955,
+						ends: 1030,
+						duration: 35
+					}
+				},
+				{
+					title: 'X',
+					starts: '10:40',
+					ends: '11:15 am',
+					color: 'X',
+					numbers: {
+						starts: 1040,
+						ends: 1115,
+						duration: 35
+					}
+				},
+				{
+					title: 'F - 1/2  ' + this.state.block['F'],
+					starts: '11:30',
+					ends: '12:05 pm',
+					color: 'F',
+					numbers: {
+						starts: 1130,
+						ends: 1205,
+						duration: 35
+					}
+				},
+				{
+					title: 'Lunch',
+					starts: '12:10',
+					ends: '12:40 pm',
+					color: 'L',
+					numbers: {
+						starts: 1210,
+						ends: 1240,
+						duration: 30
+					}
+				},
+				{
+					title: 'F - 2/2  ' + this.state.block['F'],
+					starts: '12:45',
+					ends: '1:25 pm',
+					color: 'F',
+					numbers: {
+						starts: 1245,
+						ends: 1325,
+						duration: 40
+					}
+				},
+				{
+					title: 'G  ' + this.state.block['G'],
+					starts: '1:40',
+					ends: '3:00 pm',
+					color: 'G',
+					numbers: {
+						starts: 1340,
+						ends: 1500,
+						duration: 80
+					}
+				}
+			],
+			[
+				{
+					title: 'A  ' + this.state.block['A'],
+					starts: '8:20',
+					ends: '8:45 am',
+					color: 'A',
+					numbers: {
+						starts: 820,
+						ends: 845,
+						duration: 25
+					}
+				},
+				{
+					title: 'B  ' + this.state.block['B'],
+					starts: '8:55',
+					ends: '9:20 am',
+					color: 'B',
+					numbers: {
+						starts: 855,
+						ends: 920,
+						duration: 25
+					}
+				},
+				{
+					title: 'C  ' + this.state.block['C'],
+					starts: '9:30',
+					ends: '9:55 am',
+					color: 'C',
+					numbers: {
+						starts: 930,
+						ends: 955,
+						duration: 25
+					}
+				},
+				{
+					title: 'D  ' + this.state.block['D'],
+					starts: '10:05',
+					ends: '10:30 am',
+					color: 'D',
+					numbers: {
+						starts: 1005,
+						ends: 1030,
+						duration: 25
+					}
+				},
+				{
+					title: 'E  ' + this.state.block['E'],
+					starts: '10:40',
+					ends: '11:05 am',
+					color: 'E',
+					numbers: {
+						starts: 1040,
+						ends: 1105,
+						duration: 25
+					}
+				},
+				{
+					title: 'F  ' + this.state.block['F'],
+					starts: '11:15',
+					ends: '11:40 am',
+					color: 'F',
+					numbers: {
+						starts: 1115,
+						ends: 1140,
+						duration: 25
+					}
+				},
+				{
+					title: 'G  ' + this.state.block['G'],
+					starts: '11:50',
+					ends: '12:15 pm',
+					color: 'G',
+					numbers: {
+						starts: 1150,
+						ends: 1215,
+						duration: 25
+					}
+				},
+			]
+		]
+
 
 		let scheduleForToday;
 		let today = new Date();
@@ -92,231 +349,3 @@ const colors = {
 	X: '#0d1317'
 }
 
-const schedule = [
-	[
-		{
-			title: 'A',
-			starts: '8:20',
-			ends: '9:40 am',
-			color: 'A',
-			numbers: {
-				starts: 820,
-				ends: 940,
-				duration: 80
-			}
-		},
-		{
-			title: 'B',
-			starts: '9:55',
-			ends: '11:15 am',
-			color: 'B',
-			numbers: {
-				starts: 955,
-				ends: 1115,
-				duration: 80
-			}
-		},
-		{
-			title: 'C - 1/2',
-			starts: '11:30',
-			ends: '12:05 pm',
-			color: 'C',
-			numbers: {
-				starts: 1130,
-				ends: 1205,
-				duration: 35
-			}
-		},
-		{
-			title: 'Lunch',
-			starts: '12:10',
-			ends: '12:40 pm',
-			color: 'L',
-			numbers: {
-				starts: 1210,
-				ends: 1240,
-				duration: 30
-			}
-		},
-		{
-			title: 'C - 2/2',
-			starts: '12:45',
-			ends: '1:25 pm',
-			color: 'C',
-			numbers: {
-				starts: 1245,
-				ends: 1325,
-				duration: 40
-			}
-		},
-		{
-			title: 'D',
-			starts: '1:40',
-			ends: '3:00 pm',
-			color: 'D',
-			numbers: {
-				starts: 1340,
-				ends: 1500,
-				duration: 80
-			}
-		}
-	],
-	[
-		{
-			title: 'E',
-			starts: '8:20',
-			ends: '9:40 am',
-			color: 'E',
-			numbers: {
-				starts: 820,
-				ends: 940,
-				duration: 80
-			}
-		},
-		{
-			title: 'Advisory',
-			starts: '9:55',
-			ends: '10:30 am',
-			color: 'T',
-			numbers: {
-				starts: 955,
-				ends: 1030,
-				duration: 35
-			}
-		},
-		{
-			title: 'X',
-			starts: '10:40',
-			ends: '11:15 am',
-			color: 'X',
-			numbers: {
-				starts: 1040,
-				ends: 1115,
-				duration: 35
-			}
-		},
-		{
-			title: 'F - 1/2',
-			starts: '11:30',
-			ends: '12:05 pm',
-			color: 'F',
-			numbers: {
-				starts: 1130,
-				ends: 1205,
-				duration: 35
-			}
-		},
-		{
-			title: 'Lunch',
-			starts: '12:10',
-			ends: '12:40 pm',
-			color: 'L',
-			numbers: {
-				starts: 1210,
-				ends: 1240,
-				duration: 30
-			}
-		},
-		{
-			title: 'F - 2/2',
-			starts: '12:45',
-			ends: '1:25 pm',
-			color: 'F',
-			numbers: {
-				starts: 1245,
-				ends: 1325,
-				duration: 40
-			}
-		},
-		{
-			title: 'G',
-			starts: '1:40',
-			ends: '3:00 pm',
-			color: 'G',
-			numbers: {
-				starts: 1340,
-				ends: 1500,
-				duration: 80
-			}
-		}
-	],
-	[
-		{
-			title: 'A',
-			starts: '8:20',
-			ends: '8:45 am',
-			color: 'A',
-			numbers: {
-				starts: 820,
-				ends: 845,
-				duration: 25
-			}
-		},
-		{
-			title: 'B',
-			starts: '8:55',
-			ends: '9:20 am',
-			color: 'B',
-			numbers: {
-				starts: 855,
-				ends: 920,
-				duration: 25
-			}
-		},
-		{
-			title: 'C',
-			starts: '9:30',
-			ends: '9:55 am',
-			color: 'C',
-			numbers: {
-				starts: 930,
-				ends: 955,
-				duration: 25
-			}
-		},
-		{
-			title: 'D',
-			starts: '10:05',
-			ends: '10:30 am',
-			color: 'D',
-			numbers: {
-				starts: 1005,
-				ends: 1030,
-				duration: 25
-			}
-		},
-		{
-			title: 'E',
-			starts: '10:40',
-			ends: '11:05 am',
-			color: 'E',
-			numbers: {
-				starts: 1040,
-				ends: 1105,
-				duration: 25
-			}
-		},
-		{
-			title: 'F',
-			starts: '11:15',
-			ends: '11:40 am',
-			color: 'F',
-			numbers: {
-				starts: 1115,
-				ends: 1140,
-				duration: 25
-			}
-		},
-		{
-			title: 'G',
-			starts: '11:50',
-			ends: '12:15 pm',
-			color: 'G',
-			numbers: {
-				starts: 1150,
-				ends: 1215,
-				duration: 25
-			}
-		},
-	]
-]
