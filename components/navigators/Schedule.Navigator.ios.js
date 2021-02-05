153e19701edc4ecede57b2c60eec362c824a9a25
Allen316 <74190502+Allen316@users.noreply.github.com>
2021-02-04 21:07:09 -0500

    Create Schedule.Navigator.ios.js

---
+++ components/navigators/Schedule.Navigator.ios.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import {Button, Alert, View} from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import ScheduleScreen from '../screens/Schedule.Screen';
+import CustomizationScreen from '../screens/Customization.Screen';
+import * as firebase from 'firebase';
+
+const Stack = createStackNavigator();
+
+export default class ScheduleNavigator extends React.Component {
+
+  signOut()
+  {
+    Alert.alert(
+      "Are you sure you want to sign out?",
+      "",
+      [
+        {
+        text: "Cancel",
+        style: "cancel"
+        },
+        { text: "Yes", onPress: () => firebase.auth().signOut()}
+      ],
+      { cancelable: true }
+      );
+  }
+
+  render() {    
+
+    return (
+      <Stack.Navigator>
+        <Stack.Screen component={ScheduleScreen} name="Schedule" 
+          options={{
+          headerStyle: {
+            backgroundColor: '#871609',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerRight: () => (
+            <View style={{flexDirection: 'row'}}>
+            <View style={{marginTop: 0, marginBottom: 0}}>
+            <Button
+              onPress={() => this.props.navigation.navigate("Customize")}
+              title="Customize"
+            />
+            </View>
+            <View style={{margin: 0}}>
+            <Button
+              onPress={() => this.signOut()}
+              title="Sign Out"
+            />
+            </View>
+            </View>
+          )
+          }} 
+          />
+        <Stack.Screen component={CustomizationScreen} name="Customize"
+        options={{
+          headerStyle: {
+            backgroundColor: '#871609',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+        />
+      </Stack.Navigator>
+    )
+  }
+}
