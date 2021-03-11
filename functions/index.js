d916f0d3d3dfe9baacb719a67899d2400d160bf1
yuenler <lerchow@gmail.com>
2021-03-11 10:38:23 -0500

    update functions index.js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,47 @@
 const functions = require("firebase-functions");
+var fetch = require('node-fetch')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+//send the push notification 
+exports.sendPushNotification = functions.database.ref('Announcements/').onCreate(event => {
+
+    const root = event.data.ref.root
+    var messages = []
+
+    //return the main promise 
+    return root.child('/users').once('value').then(function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+
+            var expoToken = childSnapshot.val().expoToken;
+
+            messages.push({
+                "to": expoToken,
+                "sound": "default",
+                "body": "A new announcement has been posted!"
+            });
+        });
+        //firebase.database then() respved a single promise that resolves
+        //once all the messages have been resolved 
+        return Promise.all(messages)
+
+    })
+        .then(messages => {
+            // console.log(messages)
+            fetch('https://exp.host/--/api/v2/push/send', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(messages)
+
+            });
+        })
+        .catch(reason => {
+            console.log(reason)
+        })
+
+
+});
