f6ba1d4a641ef88f7784481b57e35466ca8ae4d6
yuenler <lerchow@gmail.com>
2021-04-26 13:26:17 -0400

    location stuff

--- components/screens/Setup.Screen.js
+++ components/screens/Setup.Screen.js
@@ -2,13 +2,20 @@ import React from 'react';
 import {Button, StyleSheet, Alert, View, Text} from 'react-native';
 import {globalStyles} from '../GlobalStyles';
 import {Input} from 'react-native-elements'
+import * as Location from 'expo-location';
 import firebase from 'firebase';
 
 export default class SetupScreen extends React.Component {
 
     state={
       question: 1,
-      schoolType: ''
+      schoolType: '',
+      zipCode: '',
+      nearbySchools: [],
+    }
+
+    get schoolRef(){
+      return firebase.database().ref('publicSchools')
     }
 
     onStudent(){
@@ -37,6 +44,48 @@ export default class SetupScreen extends React.Component {
       this.setState({question: 4})
       this.state.schoolType = 'private'
     }
+
+    onGPS(){
+      this.setState({question: 5})
+    }
+
+    onAddress(){
+      this.setState({question: 6})
+    }
+
+    getLocation = async() => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+              Alert.alert('Permission to access location was denied');
+              return;
+            }
+      
+            let location = await Location.getCurrentPositionAsync({});
+            this.getNearbySchools(location.latitude, location.longitude);
+      }
+
+    getNearbySchools(latitude, longitude) {
+      this.ref.on('child_added', (snapshot) => {
+        // use distance formula to calculate distance
+        let latitudeDifference = latitude - snapshot.val().latitude
+        let longitudeDifference = longitude - snapshot.val().longitude
+
+        let latitudeDifferenceKM = latitudeDifference/110.574
+        let longitudeDifferenceKM = Math.acos(longitudeDifference/111.320)
+
+        let distance = Math.sqrt(latitudeDifferenceKM **2 + longitudeDifferenceKM**2)
+
+        // We define a nearby school as being within a 5 kilometer distance
+        if (distance < 5){
+          let nearbySchool = snapshot.val()
+          nearbySchool[distance] = distance
+          this.state.nearbySchools = this.state.nearbySchools.concat(nearbySchool)
+          this.forceUpdate()
+        }
+        
+      })
+    }
+      
 
 	  render() {
 	
@@ -116,20 +165,43 @@ export default class SetupScreen extends React.Component {
         {this.state.question==4?
           <View>
           <View style={{flex: 1, justifyContent: 'center'}}>
-          <Text>We need to find your school! My school's zip code is...</Text>
+          <Text>We need to find your school! How would you like to find your school?</Text>
           </View>
 
-          <Input placeholder="XXXXX"
-						label="Zip code"
-						// onChangeText={activities => this.setState({ activities })}
-						// value={this.state.activities}
-				/> 
+          <View style={{flex: 1}}>
+          <Button 
+          onPress={() => this.onGPS()}
+          title="Use my phone's GPS to find schools nearby"/>
+          </View>
 
-          
+          <View style={{flex: 1}}>
+          <Button 
+          onPress={() => this.onAddress()}
+          title="Enter school address"/>
+          </View>
 
           </View>
           : null
         }
+
+        {this.state.question==6?
+          <View>
+          <View style={{flex: 1, justifyContent: 'center'}}>
+          <Text>We need to find your school! My school's zip code is...</Text>
+          </View>
+
+          <View style={{flex: 1}}>
+          <Input placeholder="XXXXX"
+						label="Zip code"
+						onChangeText={zipCode => this.setState({ zipCode })}
+						value={this.state.zipCode}
+				/> 
+        </View>
+
+          </View>
+          : null
+        }
+
 
       </View>
 		);
